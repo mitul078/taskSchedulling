5d0e12bb1257a6a26ebbbb4bcb37c8e0dd08bcf8
mitul078 <mituljodhani788@gmail.com>
2025-12-08 15:42:56 +0530

    DB catch err

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 
 async function connectDB() {
     await mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("DB Connected.")
-    })
+        .then(() => {
+            console.log("DB Connected.")
+        }).catch(err => console.log("Db error: ", err))
 }
 
 module.exports = connectDB
