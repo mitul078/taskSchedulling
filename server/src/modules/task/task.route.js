92daedbd98c0c523c61e02a65d86ed2efef799f6
mitul078 <mituljodhani788@gmail.com>
2025-12-08 07:54:17 +0530

    bug

--- server/src/modules/task/task.route.js
+++ server/src/modules/task/task.route.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 
 //task CRUD
-router.post("/:id" , authMiddleware , checkRole("manager") , createTask) //assigned id
+router.post("/:id" , authMiddleware , checkRole("manager") , createTask) //assign employee id
 router.delete("/:id" , authMiddleware , checkRole("admin" , "manager") , deleteTask) // task id
 router.get("/" , authMiddleware , checkRole("employee") , getTask)
 router.patch("/:id" , authMiddleware , checkRole("employee") , updateTaskStatus) // task id
