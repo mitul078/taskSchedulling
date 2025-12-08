566a2b3cb9a565b6c16a421ca4961789722b746c
mitul078 <mituljodhani788@gmail.com>
2025-12-08 16:18:18 +0530

    express-rate-limit implement

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
+const rateLimit = require("express-rate-limit")
+
 
 app.use(express.json())
 app.use(cookieParser())
 
+const apiLimiter = rateLimit({
+    windowMs: 10 * 1000,
+    max:5,
+    message: "Too many requests"
+})
+
+app.use(apiLimiter)
 
 app.use("/api/v1/auth" , require("./modules/auth/auth.route"))
 app.use("/api/v1/admin" , require("./modules/admin/admin.route"))
